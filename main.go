872f9b60e6e4623925d203419a5dc149eb2a233a
Negai Marin <negaimarin@gmail.com>
2026-02-20 21:39:17 +0200

    feat: add base main structure

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: task [add|list|done|delete] [args]")
+		return
+	}
+
+	command := os.Args[2]
+
+	switch command {
+	case "add":
+		//TODO
+	case "list":
+		//TODO
+	case "done":
+		//TODO
+	case "delete":
+		//TODO
+	default:
+		fmt.Println("unkown command:", command)
+	}
 }
